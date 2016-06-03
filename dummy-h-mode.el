2bc896f0e3bd3c976c4bb4cdf8056065bf39f50e
yascentur <yascentur@gmail.com>
2016-06-03 20:17:23 +0900

    Revise description

--- dummy-h-mode.el
+++ dummy-h-mode.el
@@ -22,12 +22,12 @@
 ;;; Commentary:
 
 ;; The `dummy-h-mode' is a major mode for header files (.h)
-;; of C, C++ or Objective-C.  It detects the suitable major mode
-;; which is one of `c-mode', `c++-mode' and `objc-mode',
-;; then switches the buffer major mode to the detected major mode.
-;; The detecting process is as follows:
+;; of C, C++ and Objective-C.  It detects the suitable major mode
+;; that is one of `c-mode', `c++-mode' and `objc-mode',
+;; and then switches the buffer major mode to the detected major mode.
+;; The detecting process is described as follows:
 ;;
-;; 1. Check the existance of the corresponding source file (.c, .cc or ...)
+;; 1. Check the existance of the corresponding source file (.c, .cc, etc.)
 ;; 2. Search keywords in the file.
 ;; 3. Check the filename extensions of all files in the directory.
 
